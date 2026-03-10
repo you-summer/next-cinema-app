1f036b2ee97287a5fa308215c1957c12c40e9e07
조연정 <jojoor201@gmail.com>
2026-03-10 18:35:56 +0900

    캐시옵션

--- src/app/(with-searchbar)/page.tsx
+++ src/app/(with-searchbar)/page.tsx
@@ -1,25 +1,60 @@
 import MovieItem from "@/components/movie-item";
 import style from "./page.module.css";
 import movies from "@/dummy.json";
+import { MovieData } from "@/types";
+
+async function AllMovies() {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/movie`,
+    { cache: "force-cache" }, // 요청의 결과를 무조건 캐싱함 이유 : 한번 호출된 이후에는 다시는 호출하지 않아서 웬만하면 업데이트가 없어서?
+  );
+
+  if (!response.ok) {
+    return <div>오류가 발생했습니다 ...</div>;
+  }
+
+  const allMovies: MovieData[] = await response.json();
+
+  return (
+    <div className={style.all_container}>
+      {allMovies.map((movie) => (
+        <MovieItem key={`all-${movie.id}`} {...movie} />
+      ))}
+    </div>
+  );
+}
+
+async function RecoMovie() {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/movie/random`,
+    { next: { revalidate: 3 } },
+  );
+
+  if (!response.ok) {
+    return <div>오류가 발생했습니다...</div>;
+  }
+
+  const recoMovie: MovieData[] = await response.json();
+
+  return (
+    <div className={style.reco_conatiner}>
+      {recoMovie.map((movie) => (
+        <MovieItem key={`reco-${movie.id}`} {...movie} />
+      ))}
+    </div>
+  );
+}
 
 export default function Home() {
   return (
     <div className={style.conatiner}>
       <section>
         <h3>지금 가장 추천하는 영화</h3>
-        <div className={style.reco_conatiner}>
-          {movies.slice(0, 3).map((movie) => (
-            <MovieItem key={`reco-${movie.id}`} {...movie} />
-          ))}
-        </div>
+        <RecoMovie />
       </section>
       <section>
         <h3>등록된 모든 영화</h3>
-        <div className={style.all_container}>
-          {movies.map((movie) => (
-            <MovieItem key={`all-${movie.id}`} {...movie} />
-          ))}
-        </div>
+        <AllMovies />
       </section>
     </div>
   );
